20a99266a39d594118cd14c80b9d92fb97c2767c
sarra zidi <sarra.zidi@enicar.ucar.tn>
2026-09-04 14:29:40 +0100

    ajout  dun commentaire de test

--- bootcamp_devops/src/main/java/com/sip/app/BootcampDevopsApplication.java
+++ bootcamp_devops/src/main/java/com/sip/app/BootcampDevopsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootcampDevopsApplication {
+	//test de commit
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampDevopsApplication.class, args);
