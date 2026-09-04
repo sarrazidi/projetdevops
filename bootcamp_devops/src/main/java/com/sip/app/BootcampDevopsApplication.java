4b8bb6f1d76e2d70b7c0e05cf3c8e0ad935acb23
sarra zidi <sarra.zidi@enicar.ucar.tn>
2026-09-04 12:55:11 +0100

    Initial commit

---
+++ bootcamp_devops/src/main/java/com/sip/app/BootcampDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.sip.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDevopsApplication.class, args);
+	}
+
+}
---
+++ bootcamp_devops/src/test/java/com/sip/app/BootcampDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sip.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
